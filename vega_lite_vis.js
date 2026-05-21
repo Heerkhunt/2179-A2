6df30c3863f4058650dbd7d338cc5f8889693d26
Heer Khunt <99044201+Heerkhunt@users.noreply.github.com>
2026-05-22 03:18:12 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,16 @@
+const charts = [
+  ["#vis1", "js/vis1_choropleth.vg.json"],
+  ["#vis2", "js/vis2_dot_density.vg.json"],
+  ["#vis3", "js/vis3_lollipop.vg.json"],
+  ["#vis4", "js/vis4_heatmap.vg.json"],
+  ["#vis5", "js/vis5_small_multiples.vg.json"],
+  ["#vis6", "js/vis6_dumbbell.vg.json"],
+  ["#vis7", "js/vis7_streamgraph.vg.json"],
+  ["#vis8", "js/vis8_horizon.vg.json"],
+  ["#vis9", "js/vis9_treemap.vg.json"],
+  ["#vis10", "js/vis10_connected_scatter.vg.json"],
+  ["#vis11", "js/vis11_flow_map.vg.json"],
+  ["#vis12", "js/vis12_network.vg.json"],
+  ["#vis13", "js/vis13_custom_combo.vg.json"]
+];
+charts.forEach(([id, spec]) => vegaEmbed(id, spec, {actions:false}).catch(console.error));
